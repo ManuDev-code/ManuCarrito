9689e6e845cc9b4154ab59f4a258bb2ce4d920c7
Manuela <manuelamunozlujan0@gmail.com>
2025-03-08 11:15:40 -0500

    agregando descripcion a productos

--- js/Productos.js
+++ js/Productos.js
@@ -11,41 +11,48 @@ const Productos = [
         nombre: "iPhone",
         img: "../img/iPhone.jpg",
         precio: 4000000,
+        descripcion: "Dispositivo de alta tecnología",
      },
      {
         id: 3,
         nombre: "iPad",
         img: "../img/iPad1.webp",
         precio: 5000000,
+        descripcion: "Dispositivo para entretener a tus hijos",
      },
      {
         id: 4,
         nombre: "AirPods",
         img: "../img/AirPods.webp",
         precio: 2000000,
+        descripcion: "Aislador del mundo",
      },
      {
         id: 5,
         nombre: "watch",
         img: "../img/watch.webp",
         precio: 3000000,
+        descripcion: "Súper reloj",
      },
      {
         id: 6,
         nombre: "Studio",
         img: "../img/Studio.webp",
         precio: 3000000,
+        descripcion: "Mega estudio con muchas funcionalidades",
      },
      {
         id: 7,
         nombre: "TV",
         img: "../img/TVyhogar.webp",
         precio: 3000000,
+        descripcion: "Todas las series al alcance de tus manos",
      },
      {
         id: 8,
         nombre: "Audífonos",
         img: "../img/Audífonos.webp",
         precio: 3000000,
+        descripcion: "Increíble calidad",
      }
 ]
